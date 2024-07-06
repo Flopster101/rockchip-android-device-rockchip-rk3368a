54d0d751cbfee36f2522fa74fdaa7a9df1b04eab
Joel Gómez <nahuelgomez329@gmail.com>
2024-07-06 15:56:04 -0300

    fstab: properly copy fstab, use block devices instead of by-name
    Signed-off-by: Joel Gómez <nahuelgomez329@gmail.com>

--- device.mk
+++ device.mk
@@ -39,6 +39,7 @@ endif
     vendor/rockchip/common/bin/$(TARGET_ARCH)/busybox:recovery/root/sbin/busybox \
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fstab.rk30board:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.rk30board \
     $(LOCAL_PATH)/init.rk3368.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.rk3368.rc \
     $(LOCAL_PATH)/init.rk3368.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.rk3368.usb.rc \
     $(LOCAL_PATH)/wake_lock_filter.xml:system/etc/wake_lock_filter.xml \
